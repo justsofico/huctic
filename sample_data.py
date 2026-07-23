ee181975e19fbd35a4f8c430f396dbc1bfdf6c2f
justsofico <justsofi@ukr.net>
2026-07-23 17:15:23 +0300

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,53 @@
+from models import Artifact
+
+ARTIFACTS = [
+
+    Artifact(
+        "Antikythera Mechanism",
+        "Ancient Greece",
+        "Astronomy"
+    ),
+
+    Artifact(
+        "Roman Sundial",
+        "Roman Empire",
+        "Science"
+    ),
+
+    Artifact(
+        "Astrolabe",
+        "Middle Ages",
+        "Navigation"
+    ),
+
+    Artifact(
+        "Mechanical Clock",
+        "Renaissance",
+        "Engineering"
+    ),
+
+    Artifact(
+        "Early Telegraph",
+        "Industrial Age",
+        "Communication"
+    ),
+
+    Artifact(
+        "Bronze Helmet",
+        "Ancient Greece",
+        "Military"
+    ),
+
+    Artifact(
+        "Steam Engine Model",
+        "Industrial Age",
+        "Engineering"
+    ),
+
+    Artifact(
+        "Printing Press",
+        "Renaissance",
+        "Technology"
+    )
+
+]
